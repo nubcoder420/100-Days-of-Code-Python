3bcc6bd83f4fd095f103015891a962c36b3fbe0f
nubcoder420 <138780259+nubcoder-420@users.noreply.github.com>
2023-07-22 17:05:29 +0800

    Update Coffee Machine.py

--- Coffee Machine.py
+++ Coffee Machine.py
@@ -31,3 +31,61 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
+
+
+quarters = 0.25
+dimes = 0.1
+nickles = 0.05
+pennies = 0.01
+
+
+water = resources['water']
+milk = resources['milk']
+coffee = resources['coffee']
+
+
+def espresso():
+    espresso_water = MENU['espresso']['ingredients']['water']
+    espresso_coffee = MENU['espresso']['ingredients']['coffee']
+    espresso_cost = MENU['espresso']['cost']
+    return espresso_water, espresso_coffee, espresso_cost
+
+def latte():
+    latte_water = MENU['latte']['ingredients']['water']
+    latte_milk = MENU['latte']['ingredients']['milk']
+    latte_coffee = MENU['latte']['ingredients']['coffee']
+    latte_cost = (MENU['latte']['cost'])
+    return latte_water, latte_milk, latte_coffee, latte_cost
+
+
+def cappuccino():
+    cappuccino_water = MENU['cappuccino']['ingredients']['water']
+    cappuccino_milk = MENU['cappuccino']['ingredients']['milk']
+    cappuccino_coffee = MENU['cappuccino']['ingredients']['coffee']
+    cappuccino_cost = (MENU['cappuccino']['cost'])
+    return cappuccino_water, cappuccino_milk, cappuccino_coffee, cappuccino_cost
+
+espresso_water, espresso_coffee, espresso_cost = espresso()
+latte_water, latte_milk, latte_coffee, latte_cost = latte()
+cappuccino_water, cappuccino_milk, cappuccino_coffee, cappuccino_cost = cappuccino()
+
+def input_money():
+    input_quarters = float(input('Enter how many quarters: '))
+    input_dimes = float(input('Enter how many dimes: '))
+    input_nickles = float(input('Enter how many nickles: '))
+
+# ordered_drink = input("What would you like? (espresso/latte/cappuccino): ").lower()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
