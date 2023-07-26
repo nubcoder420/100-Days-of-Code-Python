22ace6f8eb032bdc3f4cee188ecb800b3650b64d
nubcoder420 <138780259+nubcoder-420@users.noreply.github.com>
2023-07-26 17:54:10 +0800

    Update quiz_brain.py
    added still_has_questions() method

--- 156 quiz-game-start/quiz_brain.py
+++ 156 quiz-game-start/quiz_brain.py
@@ -1,9 +1,14 @@
 class QuizBrain:
+    
 
     def __init__(self, question_list):
 
         self.question_number = 0
         self.question_list = question_list
+
+
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
 
 
     def next_question(self):
