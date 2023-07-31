030b548d55c0656a1d5cabb32f4143b3b5db5907
nubcoder420 <138780259+nubcoder-420@users.noreply.github.com>
2023-07-31 09:32:53 +0800

    Update food.py
    added 'refresh' method

--- Day 20 - Snake Game /food.py
+++ Day 20 - Snake Game /food.py
@@ -10,6 +10,9 @@ class Food(Turtle):
         self.penup()
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.speed('fastest')
+        self.refresh()
+
+    def refresh(self):
         rand_x_pos = random.randint(-280, 280)
         rand_y_pos = random.randint(-280, 280)
         self.goto(rand_x_pos, rand_y_pos)
