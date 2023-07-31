60165ceac041907962b71ce573f9bd3c7543770c
nubcoder420 <138780259+nubcoder-420@users.noreply.github.com>
2023-07-31 10:56:52 +0800

    Update main.py
    added Display keyboard controls

--- Day 20 - Snake Game /main.py
+++ Day 20 - Snake Game /main.py
@@ -14,6 +14,15 @@ snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
 
+# Display keyboard controls
+controls = Turtle()
+controls.penup()
+controls.hideturtle()
+controls.color('white')
+controls.goto(0, -280)
+controls.write("UP = W, DOWN = S, LEFT = A, RIGHT = D", align='center', font=('Courier', 14, 'bold'))
+
+
 screen.listen()
 screen.onkeypress(snake.up, "w")
 screen.onkeypress(snake.down, "s")
