0c8de5a58b4657ac47b79290708dd619b0e57a90
nubcoder420 <138780259+nubcoder-420@users.noreply.github.com>
2023-07-31 09:22:59 +0800

    Update food.py
    - create class Food

--- Day 20 - Snake Game /food.py
+++ Day 20 - Snake Game /food.py
@@ -1,1 +1,15 @@
+from turtle import Turtle
+import random
 
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape('square')
+        self.color('white')
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.speed('fastest')
+        rand_x_pos = random.randint(-280, 280)
+        rand_y_pos = random.randint(-280, 280)
+        self.goto(rand_x_pos, rand_y_pos)
