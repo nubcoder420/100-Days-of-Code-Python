dd0dae92923028e3cfbe83c1fedb5df154c10d5f
nubcoder420 <138780259+nubcoder-420@users.noreply.github.com>
2023-08-01 09:10:22 +0800

    Update scoreboard.py

--- Day 23 - Turtle Crossing Game/scoreboard.py
+++ Day 23 - Turtle Crossing Game/scoreboard.py
@@ -1,5 +1,22 @@
-FONT = ("Courier", 24, "normal")
+from turtle import Turtle
+
+FONT = ("Courier", 14, "bold")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+
+        self.penup()
+        self.hideturtle()
+        self.level = 1
+
+    def update_level(self):
+        self.clear()
+        self.goto(-240, 260)
+        self.write(f"Level: {self.level}", align='center', font=FONT)
+
+    def add_level(self):
+        self.level += 1
+
