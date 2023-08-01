412c6a939bac2c0e87e676f10e385ab44a2f7939
nubcoder420 <138780259+nubcoder-420@users.noreply.github.com>
2023-08-01 09:10:52 +0800

    Update main.py

--- Day 23 - Turtle Crossing Game/main.py
+++ Day 23 - Turtle Crossing Game/main.py
@@ -1,4 +1,5 @@
 import time
+import random
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
@@ -6,13 +7,32 @@ from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
-# screen.screensize(canvwidth=600, canvheight=600)
 screen.tracer(0)
 
+player = Player()
+scoreboard = Scoreboard()
+car_manager = CarManager()
+
+screen.listen()
+screen.onkeypress(fun=player.move_up, key="Up")
+
 game_is_on = True
+
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    scoreboard.update_level()
+
+    car_manager.move()
+
+    # Frequency of generated cars
+    if random.randint(1, 6) == 1:
+        car_manager.generate_car()
+
+    if player.ycor() > 280:  # 280 finish line
+        scoreboard.add_level()
+        player.reset_position()
+        car_manager.increase_speed()
 
 
 screen.exitonclick()
