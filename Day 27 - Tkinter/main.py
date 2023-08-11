c4b0eb1f1d44c0a5935310c9c86395ef66198baa
nubcoder420 <138780259+nubcoder-420@users.noreply.github.com>
2023-08-11 16:39:07 +0800

    Update main.py

--- Day 27 - Tkinter/main.py
+++ Day 27 - Tkinter/main.py
@@ -1,1 +1,43 @@
-import tkinter
+from tkinter import *
+
+WIDTH = 500
+HEIGHT = 500
+
+#window
+window = Tk()
+window.title("nubcoder420")
+window.minsize(width=WIDTH, height=HEIGHT)
+
+#label
+my_label = Label(text="My Label", font=("Arial", 24, "bold"))
+my_label.pack()
+
+my_label.config(text="New Label")
+
+#button
+
+def button_clicked():
+
+    new_text = user_input.get()
+    my_label.config(text=new_text)
+    print("I got clicked.")
+
+
+my_button = Button(text="Click Me", command=button_clicked)
+my_button.pack()
+
+
+#entry
+
+user_input = Entry(width=20)
+user_input.pack()
+user_input.get()
+
+
+
+
+
+
+
+
+window.mainloop()
