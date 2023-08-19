e9623c18d00dc1eaf4f752ab0c49098750ed1b60
nubcoder420 <138780259+nubcoder-420@users.noreply.github.com>
2023-08-19 15:29:26 +0800

    Update main.py

--- Day 29 - Password Manager App/main.py
+++ Day 29 - Password Manager App/main.py
@@ -1,21 +1,51 @@
 from tkinter import *
+from tkinter import messagebox
+from random import randint, choice, shuffle
+import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+# Password Generator Project
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    pass_word = "".join(password_list)
+    password_entry.insert(0, pass_word)
+    pyperclip.copy(pass_word)
+
+    # print(f"Your password is: {pass_word}")
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def save():
-
     website = website_entry.get()
     username = username_entry.get()
     password = password_entry.get()
 
-    data_file = open("data.txt", "a")
-    data_file.write(f"{website} | {username} | {password}\n")
-    data_file.close()
+    if website == "" or username == "" or password == "":
+        messagebox.showerror(title="Oops!", message="Please don't leave any fields empty")
+    else:
+        is_ok = messagebox.askyesno(title="Proceed?",
+                                    message=f"Website: {website}\nEmail/Username: {username}\nPassword: {password}")
 
-    website_entry.delete(0, END)
-    password_entry.delete(0, END)
+        if is_ok:
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"{website} | {username} | {password}\n")
+
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -48,11 +78,10 @@ password_label.grid(row=4, column=0)
 password_entry = Entry(width=36)
 password_entry.grid(row=4, column=1)
 
-generate_button = Button(text="Generate Password")
+generate_button = Button(text="Generate Password", command=generate_password)
 generate_button.grid(row=4, column=2)
 
 add_button = Button(text="Add", width=47, command=save)
 add_button.grid(row=5, column=1, columnspan=2)
-
 
 window.mainloop()
