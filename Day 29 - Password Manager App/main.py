ef31227e813d2ea5363ea90c9288f143a2d63892
nubcoder420 <138780259+nubcoder-420@users.noreply.github.com>
2023-08-19 14:10:35 +0800

    Update main.py

--- Day 29 - Password Manager App/main.py
+++ Day 29 - Password Manager App/main.py
@@ -4,11 +4,24 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+def save():
+
+    website = website_entry.get()
+    username = username_entry.get()
+    password = password_entry.get()
+
+    data_file = open("data.txt", "a")
+    data_file.write(f"{website} | {username} | {password}\n")
+    data_file.close()
+
+    website_entry.delete(0, END)
+    password_entry.delete(0, END)
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Password Manager by nubcoder420")
-window.config(padx=20, pady=20)
+window.config(padx=30, pady=30)
 
 canvas = Canvas(width=200, height=200)
 img = PhotoImage(file="logo.png")
@@ -20,12 +33,14 @@ website_label.grid(row=2, column=0)
 
 website_entry = Entry(width=55)
 website_entry.grid(row=2, column=1, columnspan=2)
+website_entry.focus()
 
 username_label = Label(text="Email/Username:")
 username_label.grid(row=3, column=0)
 
 username_entry = Entry(width=55)
 username_entry.grid(row=3, column=1, columnspan=2)
+username_entry.insert(0, "username@email.com")
 
 password_label = Label(text="Password:")
 password_label.grid(row=4, column=0)
@@ -36,19 +51,8 @@ password_entry.grid(row=4, column=1)
 generate_button = Button(text="Generate Password")
 generate_button.grid(row=4, column=2)
 
-add_button = Button(text="Add", width=47)
+add_button = Button(text="Add", width=47, command=save)
 add_button.grid(row=5, column=1, columnspan=2)
-
-
-
-
-
-
-
-
-
-
-
 
 
 window.mainloop()
