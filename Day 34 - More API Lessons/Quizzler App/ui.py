e03d322589609b1d4d9aa551e6e571062de7e495
nubcoder420 <138780259+nubcoder-420@users.noreply.github.com>
2023-08-31 16:47:40 +0800

    Update ui.py

--- Day 34 - More API Lessons/Quizzler App/ui.py
+++ Day 34 - More API Lessons/Quizzler App/ui.py
@@ -8,7 +8,7 @@ class QuizInterface:
 
     def __init__(self, quiz_brain: QuizBrain):
         self.quiz = quiz_brain
-        
+
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -25,16 +25,16 @@ class QuizInterface:
         )
 
         self.score = Label(text="Score: 0", highlightthickness=0, bg=THEME_COLOR, fg="white")
-        self.score.config(pady=20,padx=20)
+        self.score.config(pady=20, padx=20)
         self.score.grid(row=0, column=1)
 
         self.true_img = PhotoImage(file="./images/true.png")
-        self.true_button = Button(image=self.true_img, highlightthickness=0)
+        self.true_button = Button(image=self.true_img, highlightthickness=0, command=self.true)
         self.true_button.grid(row=2, column=0)
         self.true_button.config(padx=20, pady=20)
 
         self.false_img = PhotoImage(file="./images/false.png")
-        self.false_button = Button(image=self.false_img, highlightthickness=0)
+        self.false_button = Button(image=self.false_img, highlightthickness=0, command=self.false)
         self.false_button.grid(row=2, column=1)
         self.false_button.config(padx=20, pady=20)
 
@@ -43,9 +43,27 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question, text=q_text)
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question, text=q_text)
+            self.score.config(text=f"Score: {self.quiz.score}")
+        else:
+            self.canvas.itemconfig(self.question, text="End of Quiz.")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
 
+    def true(self):
+        is_right = self.quiz.check_answer(user_answer="True")
+        self.give_feedback(is_right)
 
+    def false(self):
+        self.give_feedback(self.quiz.check_answer(user_answer="False"))
 
-    
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+
+        self.window.after(1000, self.get_next_question)
