ec97941fa1f83cf980fbfb437696267ece81d24d
nubcoder420 <138780259+nubcoder-420@users.noreply.github.com>
2023-10-19 12:14:37 +0800

    Update main.py

--- Day 64 - Advanced Flask Project/main.py
+++ Day 64 - Advanced Flask Project/main.py
@@ -7,7 +7,7 @@ from wtforms.validators import DataRequired, InputRequired, NumberRange
 import requests
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = 'SECRET KEY'
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///top-movies.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 Bootstrap(app)
