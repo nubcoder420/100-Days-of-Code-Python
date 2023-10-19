623ce1d246d05c5e4bf8855d271d6b3610172e1a
nubcoder420 <138780259+nubcoder-420@users.noreply.github.com>
2023-10-19 12:14:00 +0800

    Add files via upload

---
+++ Day 64 - Advanced Flask Project/main.py
@@ -0,0 +1,91 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, FloatField
+from wtforms.validators import DataRequired, InputRequired, NumberRange
+import requests
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///top-movies.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+Bootstrap(app)
+db = SQLAlchemy(app)
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    rating = db.Column(db.Float, nullable=True)
+    ranking = db.Column(db.Integer, nullable=True)
+    review = db.Column(db.String(250), nullable=True)
+    img_url = db.Column(db.String(250), nullable=False)
+
+class RateMovieForm(FlaskForm):
+    rating = StringField("Your Rating Out of 10 e.g. 7.5", validators=[DataRequired()])
+    review = StringField("Your Review", validators=[DataRequired()])
+    submit = SubmitField("Done")
+
+class AddMovieForm(FlaskForm):
+    title = StringField("Movie Title", validators=[DataRequired()])
+    submit = SubmitField('Add Movie')
+
+# new_movie = Movie(
+#     title="Phone Booth",
+#     year=2002,
+#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+#     rating=7.3,
+#     ranking=10,
+#     review="My favourite character was the caller.",
+#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+# )
+#
+# with app.app_context():
+#     db.create_all()
+#     db.session.add(new_movie)
+#     db.session.commit()
+
+@app.route("/")
+def home():
+    movies = Movie.query.all()
+    return render_template("index.html", movies=movies)
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    form = RateMovieForm()
+    movie_id = request.args.get("id")
+    movie = Movie.query.get(movie_id)
+
+    if form.validate_on_submit():
+        movie.rating = float(form.rating.data)
+        movie.review = form.review.data
+        db.session.commit()
+
+        return redirect(url_for('home'))
+
+    return render_template("edit.html", movie=movie, form=form)
+
+@app.route('/delete')
+def delete():
+    movie_id = request.args.get("id")
+    movie = Movie.query.get(movie_id)
+
+    if movie:
+        db.session.delete(movie)
+        db.session.commit()
+
+    return redirect(url_for('home'))
+
+@app.route('/add', methods=["GET", "POST"])
+def add():
+    form = AddMovieForm()
+
+    if form.validate_on_submit():
+        return redirect(url_for('home'))
+
+    return render_template("add.html", form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
