13bc961ff1ee8bc2c0de031a60cdc6342bd4348a
nubcoder420 <138780259+nubcoder-420@users.noreply.github.com>
2023-10-21 13:32:35 +0800

    Update main.py

--- Day 64 - Advanced Flask Project/main.py
+++ Day 64 - Advanced Flask Project/main.py
@@ -7,7 +7,7 @@ from wtforms.validators import DataRequired
 import requests
 
 
-MOVIE_DB_API_KEY = 'e5d814059aafe5a4bdbc1d7d0e572e23'
+MOVIE_DB_API_KEY = 'API KEY'
 MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
 MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
 MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
